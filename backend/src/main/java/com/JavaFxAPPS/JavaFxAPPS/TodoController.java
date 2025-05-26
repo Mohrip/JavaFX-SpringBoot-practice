c24e20fb119d6c71506d041eb6395e817f24f15e
mohrip <m25srt@gmail.com>
2025-05-26 14:49:21 +0300

    feat(api): expose endpoint for frontend integration

---
+++ backend/src/main/java/com/JavaFxAPPS/JavaFxAPPS/TodoController.java
@@ -0,0 +1,26 @@
+package com.JavaFxAPPS.JavaFxAPPS;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todo")
+
+public class TodoController {
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public List<Todo> getAllTodos() {
+        return todoService.findAll();
+    }
+
+    @PostMapping
+    public Todo addTodo(@RequestBody Todo todo) {
+        return todoService.add(todo);
+    }
+}
